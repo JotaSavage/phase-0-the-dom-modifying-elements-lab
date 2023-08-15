e3689f1152a82f66fc05719438a0e54c4673b41f
Juan Lugo <juanlugodev@gmail.com>
2023-08-14 20:03:02 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+    mainElement.remove();
+}
+
+// Creating and appending a new header with id and text
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "YOUR-NAME is the champion"; // Replace YOUR-NAME with your actual name
+document.body.appendChild(newHeader);
